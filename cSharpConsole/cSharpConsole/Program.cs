6622904524d5ccd7ee7ba44dcd4c8a0d8791c9ca
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-12-21 00:57:04 -0500

    Csharp

---
+++ cSharpConsole/cSharpConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace cSharpConsole{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List is running...");
+            linkedList list = new linkedList();
+            
+            list.insert("jonson");
+            list.insert("Mick");
+            list.insert("Tushar");
+            Console.WriteLine(list);
+        }
+    }
+}
---
+++ demo/demo/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello";
+
+//hold out-put;
+	cin.clear();
+	cin.get();
+	cin.ignore();
+}
